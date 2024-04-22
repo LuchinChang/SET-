3c8dd41332b68f2a9a651df978fb538336a7d6dc
lcchang <lcchang0000@gmail.com>
2024-04-22 09:10:37 +0800

    Initial Commit

---
+++ SET!/SET_App.swift
@@ -0,0 +1,17 @@
+//
+//  SET_App.swift
+//  SET!
+//
+//  Created by LuChin Chang on 2024/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SET_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
